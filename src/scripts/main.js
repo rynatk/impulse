ca1a43814ac3915bd78cb2b71adcd1458f7bba37
Ryan Atkinson <ryanatk@vt.edu>
2015-10-22 10:08:59 -0400

    "Init"

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import _ from 'underscore';
+import Backbone from 'backbone';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+
+$(() => {
+
+});
